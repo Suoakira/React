8b611bc88696ead73a6de5db0654b5d14f5a34c7
Suoakira <37964234+Suoakira@users.noreply.github.com>
2018-07-02 10:38:54 +0100

    Add files via upload

--- ComponentsInteract.jsx
+++ ComponentsInteract.jsx
@@ -35,3 +35,24 @@ class ProfilePage extends React.Component {
     );
   }
 }
+
+// Component Rendering in Action
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NavBar } from './NavBar';
+
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>All About Me!</h1>
+        <p>I like movies and blah blah blah blah blah</p>
+        <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProfilePage />, document.getElementById('app'));
